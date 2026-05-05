c88c9c3b0e5e8acd9769128262079a4c3716ab73
Mark Ogujiuba <markogujiuba@gmail.com>
2026-05-05 10:50:40 +0100

    baby

--- lib/storage.ts
+++ lib/storage.ts
@@ -1,15 +1,15 @@
 import fs from "fs";
 import path from "path";
-import { Booking } from "@/lib/types/booking";
+import { BookingFormState } from "@/lib/types/booking";
 
 const file = path.join(process.cwd(), "data.json");
 
-export function readBookings(): Booking[] {
+export function readBookings(): BookingFormState[] {
   if (!fs.existsSync(file)) return [];
   return JSON.parse(fs.readFileSync(file, "utf-8"));
 }
 
-export function saveBooking(b: Booking) {
+export function saveBooking(b: BookingFormState) {
   const data = readBookings();
   data.push(b);
   fs.writeFileSync(file, JSON.stringify(data, null, 2));
