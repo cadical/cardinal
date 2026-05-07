23e51cf472b390b5f5c3542ab5dff83d3d0b200e
Mark Ogujiuba <markogujiuba@gmail.com>
2026-05-07 13:56:45 +0100

    done

--- app/api/institutions/[id]/route.ts
+++ app/api/institutions/[id]/route.ts
@@ -1,7 +1,8 @@
 import prisma from "@/lib/prisma";
 
 // GET single institution
-export async function GET(_: Request, { params }: { params: { id: string } }) {
+export async function GET(_: Request, context: { params: Promise<{ id: string }> }) {
+  const params = await context.params;
   const institution = await prisma.institution.findUnique({
     where: { id: params.id },
     include: { documents: true },
@@ -11,7 +12,8 @@ export async function GET(_: Request, { params }: { params: { id: string } }) {
 }
 
 // UPDATE institution
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: Request, context: { params: Promise<{ id: string }> }) {
+  const params = await context.params;
   const body = await req.json();
 
   const updated = await prisma.institution.update({
@@ -23,7 +25,9 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
 }
 
 // DELETE institution
-export async function DELETE(_: Request, { params }: { params: { id: string } }) {
+export async function DELETE(_: Request, context: { params: Promise<{ id: string }> }) {
+  const params = await context.params;
+
   await prisma.institution.delete({
     where: { id: params.id },
   });
