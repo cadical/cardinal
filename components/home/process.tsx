89dfcec04eb6a4e6c747be867bed13b5a0191ebe
Mark Ogujiuba <markogujiuba@gmail.com>
2026-05-12 20:41:27 +0100

    process

--- components/home/process.tsx
+++ components/home/process.tsx
@@ -9,9 +9,9 @@ export default function Process() {
   return (
     <section className="px-5 py-20">
       <div className="max-w-6xl mx-auto">
-        <p className="font-serif text-sm text-[#F5A623]">HOW IT WORKS</p>
+        <p className="font-serif text-sm text-[#0d47a1]">HOW IT WORKS</p>
         <h2 className="font-serif text-3xl ">
-          Simple from start <span className="italic text-[#F5A623]">to finish.</span>
+          Simple from start <span className="italic text-[#0d47a1]">to finish.</span>
         </h2>
         <p className="font-serif text-sm mb-10">Whether you're ordering supplies or booking a service — the process is the same.</p>
 
